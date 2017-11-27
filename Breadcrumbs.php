ef84e371ca1a80215da446c731f1b96eaa420cc6
Pavel <honorhelp@gmail.com>
2017-11-27 13:01:43 +0300

    changed Breadcrumbs widget

--- Breadcrumbs.php
+++ Breadcrumbs.php
@@ -4,19 +4,10 @@
 namespace core\bootstrap;
 
 
-use core\web\Html;
-
 class Breadcrumbs extends \core\widgets\breadcrumbs\Breadcrumbs
 {
-    public function run()
-    {
-        ob_start();
-        ob_implicit_flush(false);
-        echo Html::startTag('ol', ['class' => 'crl-breadcrumb breadcrumb']);
-        foreach ($this->_elements as $key => $url){
-            echo "<li><a href=\"$url\">$key</a></li>";
-        }
-        echo Html::endTag('ol');
-        return ob_get_clean();
-    }
+    public $defaultOptions = [
+        'class' => 'crl-breadcrumb breadcrumb'
+    ];
+
 }

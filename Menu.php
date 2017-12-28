27a8e28d3728f1f013f7eac1eb215cd5f3f50660
Pavel <pfilsx@gmail.com>
2017-12-28 13:57:42 +0300

    horizontal menu

--- Menu.php
+++ Menu.php
@@ -4,18 +4,59 @@
 namespace core\bootstrap;
 
 
+use core\exceptions\ErrorException;
 use core\helpers\ArrayHelper;
 use core\web\Html;
 
 class Menu extends \core\widgets\menu\Menu
 {
-    protected function renderHorizontal()
-    {
-        echo Html::startTag('ul', ArrayHelper::merge_recursive($this->options, ['class' => 'nav nav-pills nav-justified']));
+
+    protected $_class = '';
+    protected $_class_horizontal = 'nav nav-pills';
+    protected $_class_vertical = 'list-group';
+
+    protected function render(){
+        echo Html::startTag('ul', ArrayHelper::merge_recursive($this->options, ['class' => $this->_class]));
         foreach ($this->items as $item){
-            if (!isset($item['label']) || !isset($item['url'])){
-                throw new \Exception('Invalid parameters passed to Menu::widget items');
+            if (!isset($item['label']) || (!isset($item['url']) && (!isset($item['items']) || !is_array($item['items'])))){
+                throw new ErrorException('Invalid parameters passed to Menu::widget items');
             }
+            $this->renderItem($item);
+        }
+        echo Html::endTag('ul');
+    }
+    protected function renderItem($item){
+        if ($this->orientation == 'horizontal'){
+            $this->renderHorizontalItem($item);
+        } else {
+            $this->renderVerticalItem($item);
+        }
+    }
+
+    private function renderHorizontalItem($item){
+        if (isset($item['items'])){
+
+            echo Html::startTag('li', ArrayHelper::merge_recursive([
+                'role' => 'presentation',
+                'class' => 'dropdown'
+            ],$this->itemOptions));
+            echo Html::tag('a', $item['label'].' '.Html::tag('span', ['class' => 'caret']), [
+                'class' => 'dropdown-toggle',
+                'data-toggle' => 'dropdown',
+                'href' => '#',
+                'role' => 'button',
+                'aria-haspopup' => 'true',
+                'aria-expanded' => 'false'
+            ]);
+            echo Html::startTag('ul', ['class' => 'dropdown-menu']);
+            foreach ($item['items'] as $subItem){
+                echo Html::tag('li', Html::tag('a', $subItem['label'], ['href' => $subItem['url']]),
+                    isset($subItem['options']) ? $subItem['options'] : []);
+            }
+            echo Html::endTag('ul');
+            echo Html::endTag('li');
+
+        } else {
             echo Html::startTag('li', ArrayHelper::merge_recursive($this->itemOptions, [
                 'class' => ($this->_currentUrl == $item['url'] || $this->_currentRoute == $item['url']
                     ? 'active'
@@ -25,8 +66,11 @@ class Menu extends \core\widgets\menu\Menu
             echo Html::tag('a', $item['label'], [
                 'href' => $item['url']
             ]);
+            echo Html::endTag('li');
         }
-        echo Html::endTag('ul');
+    }
+    private function renderVerticalItem($item){
+
     }
     protected function renderVertical()
     {
